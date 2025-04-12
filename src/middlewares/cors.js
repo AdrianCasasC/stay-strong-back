6d24a22223461cc6d821da642ee597d5a3c908d9
Adri-Casas <acasas@cide.net>
2025-04-12 21:38:50 +0200

    Fix: update cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,14 +2,13 @@ import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
 	'http://localhost:4200',
-	'https://4200-idx-stay-strong-front-1744357542758.cluster-blu4edcrfnajktuztkjzgyxzek.cloudworkstations.dev',
-	'https://stay-strong-back.netlify.app/.netlify/functions/api',
-	'https://stay-strong-front.netlify.app', // Cambiar por url de PRO
+	'https://stay-strong-front.netlify.app',
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
 	cors({
 		origin: (origin, callback) => {
+			console.log('Origin: ', origin);
 			if (acceptedOrigins.includes(origin)) {
 				return callback(null, true);
 			}
