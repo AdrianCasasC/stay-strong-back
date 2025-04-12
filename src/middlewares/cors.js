2138f1e6c7bbb811e53f6bf5ebc155a702482303
Adri-Casas <acasas@cide.net>
2025-04-12 21:24:17 +0200

    Fix: Add pro url to cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 const ACCEPTED_ORIGINS = [
 	'http://localhost:4200',
 	'https://4200-idx-stay-strong-front-1744357542758.cluster-blu4edcrfnajktuztkjzgyxzek.cloudworkstations.dev',
+	'https://stay-strong-back.netlify.app/.netlify/functions/api',
 	'https://stay-strong-front.netlify.app', // Cambiar por url de PRO
 ];
 
