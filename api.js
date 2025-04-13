39e4b7abeb2b93292289cb18afde4534212d4fa7
Adri-Casas <acasas@cide.net>
2025-04-13 10:11:58 +0200

    Feat: Deploy to vercel

--- api.js
+++ api.js
@@ -14,9 +14,8 @@ app.use(json());
 app.use(corsMiddleware());
 app.disable('x-powered-by');
 
-const mongoUri = process.env.MONGO_URI || Netlify.env.get('MONGO_URI');
 
-mongoose.connect(mongoUri, {
+mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	dbName: 'expense_tracker'
--- functions/api.js
+++ functions/api.js
@@ -14,9 +14,8 @@ app.use(json());
 app.use(corsMiddleware());
 app.disable('x-powered-by');
 
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://expense-tracker:Ls94f0gBap8PcRmr@cluster0.likmj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect('mongodb+srv://expense-tracker:Ls94f0gBap8PcRmr@cluster0.likmj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   dbName: 'expense_tracker'
