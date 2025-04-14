93aa668015b7e8acdf680c3f995a61702ef5209a
Adri-Casas <acasas@cide.net>
2025-04-14 08:19:10 +0200

    Fix: env variables

--- api.js
+++ api.js
@@ -18,7 +18,7 @@ app.disable('x-powered-by');
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-	dbName: 'expense_tracker'
+	dbName: process.env.MONG_DB_NAME
 })
 	.then(() => console.log('MongoDB connected'))
 	.catch(err => console.error('MongoDB connection error:', err));
