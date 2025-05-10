0bd29fc65214385770740b1ecdb51064968a04a4
AdrianCasasC <casasc.adrian@gmail.com>
2025-05-10 19:48:37 +0200

    Feat: Add update training enpoint

--- src/controllers/trainings.js
+++ src/controllers/trainings.js
@@ -3,8 +3,8 @@ import { TrainingModel } from '../models/trainings.js';
 export class TrainingController {
 	static async editTraining(req, res) {
 		const { dayId } = req.params;
-		const exercise = req.body;
-		const dayResponse = await TrainingModel.updateTraining(dayId, exercise);
+		const exercises = req.body;
+		const dayResponse = await TrainingModel.updateTraining(dayId, exercises);
 		if (dayResponse.status === 200) {
 			res.status(dayResponse.status).json(dayResponse.day);
 		} else {
