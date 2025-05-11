bc1e843b632fa9fd819c8c72c117ac51cde0bd16
AdrianCasasC <casasc.adrian@gmail.com>
2025-05-11 21:15:31 +0200

    (Fix): Endpoint name

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -7,7 +7,7 @@ export const calendarRouter = Router();
 calendarRouter.get('/', CalendarController.getAll);
 calendarRouter.post('/', CalendarController.create);
 calendarRouter.get('/day-detail/:dayId', CalendarController.getDay);
-calendarRouter.put('/day-detail/:dayId/training', TrainingController.editTraining);
+calendarRouter.put('/day-detail/training/:dayId', TrainingController.editTraining);
 calendarRouter.put('/update-day/:year/:month/:dayId', CalendarController.updateDay);
 calendarRouter.post('/update-day/:year/:month', CalendarController.addDay);
 calendarRouter.get('/prev-next', CalendarController.getCurrPrevNext);
