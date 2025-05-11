43e30e20510925d0dd4637f8cfde57fa6ad60ae7
AdrianCasasC <casasc.adrian@gmail.com>
2025-05-11 09:47:35 +0200

    (Feat): Delete file related files

--- src/routes/files.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { FilesController } from '../controllers/files.js';
-import multer from 'multer';
-
-export const filesRouter = Router();
-const upload = multer({ dest: 'uploads/' });
-
-filesRouter.post('/upload', upload.single('file'), FilesController.uploadFile);
